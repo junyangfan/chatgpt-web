f46fbd38a5a7444e85bfdcd59392662bfaea30ea
fanjunyang <junyang.fan378@imaygou.com>
2023-07-18 10:57:03 +0800

    fix: 修改作者信息

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://assets.fanjunyang.zone/avatar.png?imageMogr2/format/webp',
+      name: 'JunyangFan',
+      description: '<a href="https://blog.fanjunyang.zone" class="text-blue-500" target="_blank" >博客地址</a>',
     },
   }
 }
